6ed8c89986f4f5625b6a2dc06545c23d245a8103
Denis <125385668+denjiyy@users.noreply.github.com>
2024-09-06 18:08:23 +0300

    Add files via upload

---
+++ Models/Loan.cs
@@ -0,0 +1,45 @@
+﻿using BankManagementSystem.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankManagementSystem.Models
+{
+    public class Loan
+    {
+        public Loan()
+        {
+            Payments = new HashSet<Payment>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public LoanType LoanType { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public decimal InterestRate { get; set; }
+
+        [Required]
+        public DateOnly DateApproved { get; set; }
+
+        [Required]
+        public DateOnly StartDate { get; set; }
+
+        [Required]
+        public DateOnly EndDate { get; set; }
+
+        [Required]
+        public LoanStatus LoanStatus { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        public ICollection<Payment> Payments { get; set; }
+    }
+}
