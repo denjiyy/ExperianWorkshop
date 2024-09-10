e694727223e6f10a441f3e3ca8ca1732ce4ebf3f
Denis <125385668+denjiyy@users.noreply.github.com>
2024-09-10 10:43:38 +0300

    Add files via upload

---
+++ BankingManagementSystem.Server/Models/Card.cs
@@ -0,0 +1,47 @@
+﻿using BankManagementSystem.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankManagementSystem.Models
+{
+    public class Card
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public CardType CardType { get; set; }
+
+        private string hashedCardNumber { get; set; }
+
+        [Required]
+        public string CardNumber
+        {
+            get
+            {
+                return hashedCardNumber;
+            }
+            set
+            {
+                hashedCardNumber = BCrypt.Net.BCrypt.EnhancedHashPassword(value);
+            }
+        }
+
+        [Required]
+        public string CVV { get; set; }
+
+        [Required]
+        public DateOnly IssueDate { get; set; }
+
+        [Required]
+        public DateOnly ExpiryDate { get; set; }
+
+        [Required]
+        public Status Status { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+    }
+}
---
+++ BankingManagementSystem.Server/Models/Payment.cs
@@ -0,0 +1,32 @@
+﻿using BankManagementSystem.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankManagementSystem.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public CurrencyType CurrencyType { get; set; }
+
+        //to track any fees associated with the payment
+        public decimal Fee { get; set; }
+
+        [Required]
+        public DateOnly Date { get; set; }
+
+        [Required]
+        public int LoanId { get; set; }
+        [ForeignKey(nameof(LoanId))]
+        public Loan Loan { get; set; }
+    }
+}
