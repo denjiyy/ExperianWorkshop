dbba6cb0d5c2507f44eef7fdf70b326ab15b626e
Denis <125385668+denjiyy@users.noreply.github.com>
2024-09-13 12:44:23 +0300

    Add files via upload

--- wwwroot/Controllers/HomeController.cs
+++ wwwroot/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace BankManagementSystem.wwwroot.Controllers
 {
+    [Route("wwwroot/home/[action]")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
